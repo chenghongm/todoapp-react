be5dbebd3c8e88a57cc25153c97e76fa474e81c4
chenghong meng <mch8418@gmail.com>
2020-07-26 23:44:54 -0400

    add todolist and todoitem components

---
+++ src/todoitem/todoitem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TodoItem extends React.Component{
+
+
+    render(){
+        return(<div>Hello from the TodoItem!</div>)
+    }
+
+}
+
+export default TodoItem
---
+++ src/todolist/todolist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TodoList extends React.Component{
+
+
+    render(){
+        return(<div>Hello from the todoList!</div>)
+    }
+
+}
+
+export default TodoList
