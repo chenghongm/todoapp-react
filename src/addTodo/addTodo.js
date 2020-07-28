f9c8bb2ee5fb661f438c73690a440d1624191dbc
chenghong meng <mch8418@gmail.com>
2020-07-27 22:52:42 -0400

    add preventDefault to avoid submit empty value

--- src/addTodo/addTodo.js
+++ src/addTodo/addTodo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 class AddTodo extends React.Component{
-
+    
 
     render(){
         return(<div className='addTodoContainer'>
             
-            <form>
+            <form onSubmit={(e) => this.submitTodo(e)}>
                 <input onChange={(e) => this.updateInput(e)} type='text'></input>
-                <button type='submit'>Add Todo</button>
+                <button  type='submit'>Add Todo</button>
             </form>
            
            </div>);
@@ -16,6 +16,11 @@ class AddTodo extends React.Component{
 
     updateInput = (e) =>{
         console.log(e);
+    }
+
+    submitTodo = (e) => {
+        e.preventDefault();
+        console.log("submit");
     }
 
 }
